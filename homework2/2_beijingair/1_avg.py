ee7142fa4d560ed184c2b9438dc71e1c1c749758
Ferris_Chi <chijijun@outlook.com>
2021-12-03 22:39:25 +0800

    fix bug

--- homework2/2_beijingair/1_avg.py
+++ homework2/2_beijingair/1_avg.py
@@ -15,6 +15,10 @@ for i in tqdm(range(len(df['No']))):
     if not (df['PM_Dongsi'][i] is np.nan):
         sum += int(df['PM_Dongsi'][i])
         count += 1
+        
+    if not (df['PM_Nongzhanguan'][i] is np.nan):
+        sum += int(df['PM_Nongzhanguan'][i])
+        count += 1
 
     if count != 0:
         df.loc[i, 'avg'] = round(sum / count, 2)
